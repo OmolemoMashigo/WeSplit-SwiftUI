0cbc6b4cd3bb91f8a7e9c7caa8c95dcb1764c5e1
Omolemo Mashigo <omomashigo@gmail.com>
2024-10-26 01:39:11 +0200

    created views in a loop using ForEach

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    let students = ["Joe", "Jonn", "Jeff"]
+    @State private var selectedStudent = "Joe"
     var body: some View {
-        Form{
-            TextField("Enter your name", text: $name) //two-way binding: the value is read, but also written back
-            Text("your name is: \(name)")
+        NavigationStack{
+            Form{
+                Picker("select your student",selection: $selectedStudent){
+                    ForEach(students, id: \.self){
+                        // the id: \.self means the strings in the array are each unique and thus assigns a unique identifier to each student
+                        Text($0)
+                    }
+                }
+            }
         }
         
     }
