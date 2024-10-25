4b7b1477ea2672e8b8ee8f22808fd730e5e3b489
Omolemo Mashigo <omomashigo@gmail.com>
2024-10-25 23:31:52 +0200

    modified program state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
     var body: some View {
         NavigationStack{
             Form{
                 Text("HELLO WORLD")
+                Button("tap count: \(tapCount)"){
+                    tapCount += 1
+                }
             }
             .navigationTitle("SwiftUI")
             .navigationBarTitleDisplayMode(.inline)
