983755291abb67b1acd8948978c213b1f8c08fb1
Omolemo Mashigo <omomashigo@gmail.com>
2024-11-23 22:43:42 +0200

    added conditional modifier to turn amount text red if tip percentage is 0

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
-    
+   
     @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [0, 10, 15, 20]
@@ -21,10 +21,16 @@ struct ContentView: View {
         let tipSelection = Double(tipPercentage)
         let tipValue = (checkAmount / 100) * tipSelection
         let grandTotal = checkAmount + tipValue
+        //totalAmount = Int(grandTotal)
         let amountPerPerson = grandTotal / Double(numberOfPeople + 2)
         
         return amountPerPerson
     }
+    
+    var totalAmount: Double {
+        return totalPerPerson * Double(numberOfPeople)
+    }
+    
     var body: some View {
         NavigationStack{
             Form{
@@ -48,7 +54,16 @@ struct ContentView: View {
                     }.pickerStyle(.segmented)
                 }
                 
-                Section{
+                Section("total amount"){
+                    //TODO: fix totalAmount bug
+                    //the totalPerPerson is multiplied with the initial numberOfPeople
+                    // which is 0 and the amountPerPerson is always multiplied by 2 instead of the modified numberOfPeople
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundColor(tipPercentage == 0  ? .red : .black)
+                        
+                }
+                
+                Section("Amount per person"){
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
                 
